b9973e04e1b1cd47f6d72b162db1479e9fdd0423
ayechanhtun <ackhtun@gmail.com>
2017-03-19 10:48:16 -0400

    Update suitcase.m
    Extra parameters must be defined outside the ODE function and passed in when you specify the function handle.

--- suitcase.m
+++ suitcase.m
@@ -1,27 +1,13 @@
-function dydt = suitcase(t,y,m_s,k_s,b_s,m_l,k_l,b_l,m_c,F_i)
+function dydt = suitcase(t,y,m_s,k_s,b_s,m_l,k_l,b_l,m_c,F_i,qs,ps)
 
 dydt = zeros(5,1);
 
-% % Variables
-% % Constants for the shell, lining, and contents
-% m_s = 3;
-% k_s = 0;
-% b_s = 0;
-% m_l = 1;
-% k_l = 0;
-% b_l = 0;
-% m_c = 20;
-
-% Constants for the shell
-qs = 0;
-ps = 144;
-
-% % initial conditions of y
-% F_i = 150;
+% extra parameters must be defined outside the ODE function and passed in
+% when you specify the function handle
 
 dydt(1) = F_i - k_s*qs - b_s*((1/m_s)*ps-(1/m_l)*y(1));
-dydt(2) = k_s*y(1) + k_s*((1/m_s)*ps-(1/m_l)*y(1)) - k_l*y(2) - b_l*((1/m_l)*y(1)-(1/mc)*y(3));
-dydt(3) = k_l*y(2) + b_l*((1/m_l)*y(1)-(1/mc)*y(3));
+dydt(2) = k_s*y(1) + k_s*((1/m_s)*ps-(1/m_l)*y(1)) - k_l*y(2) - b_l*((1/m_l)*y(1)-(1/m_c)*y(3));
+dydt(3) = k_l*y(2) + b_l*((1/m_l)*y(1)-(1/m_c)*y(3));
 
 
 % dydt = zeros(5,1);
@@ -32,5 +18,3 @@ dydt(3) = k_l*y(2) + b_l*((1/m_l)*y(1)-(1/mc)*y(3));
 % dydt(3) = k_s*y(1) + k_s*y(2) - k_l*y(5) - b_l*y(6);
 % dydt(4) = (1/m_l)*y(4) - (1/m_c)*y(7);
 % dydt(5) = k_l*y(5) + b_l*y(6);
-
-
