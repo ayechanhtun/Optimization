bc4822253bc0ef11b17db24dd922e2570996311c
ayechanhtun <ackhtun@nexus.uwaterloo.ca>
2017-03-19 13:15:18 -0400

    Optimization
    Needs some work: for loop that runs the ODE solver for different th,
    runs updateParam.m

--- updateParam.m
+++ updateParam.m
@@ -1,4 +1,4 @@
-function [m_l,k_l,b_l] = updateParam(th,dth,DENSITY,YOUNGS_MOD,AREA)
+function [th,m_l,k_l,b_l] = updateParam(th,dth,DENSITY,YOUNGS_MOD_L,DAMPING_RATIO_L,AREA)
 
 % Update the value of thickness
 th = th-dth;
@@ -13,4 +13,4 @@ m_l = DENSITY*v_l;
 k_l = YOUNGS_MOD_L*AREA/th;
 
 % Damping coefficient
-b_l = DAMPING_RATIO*2*sqrt(k_l*m_l);
+b_l = DAMPING_RATIO_L*2*sqrt(k_l*m_l);
